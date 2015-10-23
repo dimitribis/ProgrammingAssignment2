e72d9a80d794421a3f86a851e72b6826237a7b65
dimitri.bisoo <dimitri.bisoo@gmail.com>
2015-10-23 02:28:13 +0200

    * Added some comments to describe those functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,15 @@
 
 ## Write a short comment describing this function
 
+## This function create an "object" ("super" matrix) containing
+## the value of the matrix and its inverse.
+## There are 4 functions :
+
+## 1. set       : set the value of the matrix
+## 2. get       : get the value of the matrix
+## 3. setinv    : set the value of the inverse
+## 4. getinv    : get the value of the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -19,6 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## This function returns the value of the inverse of the "super" matrix created
+## with the previous function.
+## For this exercise, we suppose that the matrix is always invertible.
+
+## The function will check if the value of the inverse has already been calculated.
+
+## No   : calculate the inverse, stock it in the "super matrix" and return it.
+## Yes  : print a message "getting cached data" and return it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
